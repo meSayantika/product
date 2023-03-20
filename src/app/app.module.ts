feeb16d7c0e91d637c6709ef26d3b33833c07fb1
Sayantika Dhar <sayantika.dhar04@gmail.com>
2023-03-20 18:30:08 +0530

    login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule,NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,12 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+    
+  ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  // loginForm!: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    // console.log('hi');
+    
+  }
+  logSubmit(){
+
+  }
+
+}
